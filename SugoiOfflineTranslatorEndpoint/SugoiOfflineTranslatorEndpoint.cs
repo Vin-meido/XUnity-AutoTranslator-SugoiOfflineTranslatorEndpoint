cdf40671f18d5df3eb81d3f3512ae780762eecf3
Vin-meido <86288831+Vin-meido@users.noreply.github.com>
2021-11-11 14:07:51 +0800

    Bundle the custom server and manage running/starting

--- SugoiOfflineTranslatorEndpoint/SugoiOfflineTranslatorEndpoint.cs
+++ SugoiOfflineTranslatorEndpoint/SugoiOfflineTranslatorEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,12 @@ using System.Text;
 using Newtonsoft.Json;
 using XUnity.AutoTranslator.Plugin.Core.Endpoints;
 using UnityEngine.Networking;
-
+using System.Reflection;
+using System.IO;
 
 namespace SugoiOfflineTranslator
 {
-    public class SugoiOfflineTranslatorEndpoint : ITranslateEndpoint
+    public class SugoiOfflineTranslatorEndpoint : ITranslateEndpoint, IDisposable
     {
         public string Id => "SugoiOfflineTranslatorEndpoint";
 
@@ -20,10 +22,119 @@ namespace SugoiOfflineTranslator
         public int MaxConcurrency => 1;
         public int MaxTranslationsPerRequest => 100;
 
+        private Process process;
+
+        private string AssemblyDirectory {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+
+            }
+        }
+
+        private string ServerScriptPath
+        {
+            get
+            {
+                return Path.Combine(this.AssemblyDirectory, "SugoiOfflineTranslatorServer.py");
+            }
+        }
+
+        private string ServerExecPath
+        {
+            get {
+                return Path.Combine(this.SugoiInstallPath, "backendServer\\Program-Backend\\Sugoi-Translator-Offline\\offlineTranslation");
+            }
+        }
+
+        //private string ServerPort => 14366;
+        private string ServerPort { get; set; }
+
+        //private string SugoiInstallPath => "G:\\Downloads\\Sugoi-Japanese-Translator-V3.0";
+        private string SugoiInstallPath { get; set; }
+
+
+        private string PythonExePath
+        {
+            get
+            {
+                return Path.Combine(this.SugoiInstallPath, "Power-Source\\Python38\\python.exe");
+            }
+        }
+
         public void Initialize(IInitializationContext context)
         {
             if (context.SourceLanguage != "ja") throw new Exception("Only ja is supported as source language");
             if (context.DestinationLanguage != "en") throw new Exception("Only en is supported as destination language");
+
+            this.SugoiInstallPath = context.GetOrCreateSetting("SugoiOfflineTranslator", "InstallPath", "");
+            this.ServerPort = context.GetOrCreateSetting("SugoiOfflineTranslator", "ServerPort", "14367");
+
+            if (string.IsNullOrEmpty(this.SugoiInstallPath))
+            {
+                throw new Exception("need to specify InstallPath");
+            }
+
+            this.StartProcess();
+        }
+
+        public void Dispose()
+        {
+            if (this.process != null)
+            {
+                this.SendShutdown();
+                this.process.Kill();
+                this.process.Dispose();
+                this.process = null;
+            }
+        }
+
+        private void StartProcess()
+        {
+            if (this.process == null || this.process.HasExited)
+            {
+                this.process = new Process();
+                this.process.StartInfo = new ProcessStartInfo()
+                {
+                    FileName = this.PythonExePath,
+                    Arguments = $"{this.ServerScriptPath} {this.ServerPort}",
+                    WorkingDirectory = this.ServerExecPath,
+                    UseShellExecute = false,
+                    //CreateNoWindow = true
+                };
+                this.process.Start();
+
+            }
+        }
+
+        private void SendShutdown()
+        {
+            var data = new Dictionary<string, object>()
+            {
+                { "content", "" },
+                { "message", "close server" }
+            };
+
+            var request = this.CreateRequest(data);
+            request.Send();
+        }
+
+        private UnityWebRequest CreateRequest(Dictionary<string, object> data)
+        {
+            var data_str = JsonConvert.SerializeObject(data);
+
+            var request = new UnityWebRequest(
+                $"http://localhost:{ServerPort}/",
+                "POST",
+                new DownloadHandlerBuffer(),
+                new UploadHandlerRaw(Encoding.UTF8.GetBytes(data_str)));
+
+            request.SetRequestHeader("Content-Type", "application/json");
+            request.SetRequestHeader("Accept", "*/*");
+            return request;
         }
 
         public IEnumerator Translate(ITranslationContext context)
@@ -35,17 +146,7 @@ namespace SugoiOfflineTranslator
                 { "message", "translate batch" }
             };
 
-            
-            var data_str = JsonConvert.SerializeObject(data);
-
-            var request = new UnityWebRequest(
-                "http://localhost:14366/",
-                "POST",
-                new DownloadHandlerBuffer(),
-                new UploadHandlerRaw(Encoding.UTF8.GetBytes(data_str)));
-
-            request.SetRequestHeader("Content-Type", "application/json");
-            request.SetRequestHeader("Accept", "*/*");
+            var request = CreateRequest(data);
 
             yield return request.Send();
 
---
+++ SugoiOfflineTranslatorEndpoint/SugoiOfflineTranslatorServer.py
@@ -0,0 +1,87 @@
+import os
+import sys
+
+fairseq_install_path = os.path.join(os.getcwd(), 'fairseq')
+sys.path.insert(0, fairseq_install_path)
+
+from flask import Flask
+from flask import request
+from flask_cors import CORS, cross_origin
+
+import time
+import json
+
+from fairseq.models.transformer import TransformerModel
+
+
+
+ja2en = TransformerModel.from_pretrained(
+    './fairseq/japaneseModel/',
+    checkpoint_file='big.pretrain.pt',
+    source_lang = "ja",
+    target_lang = "en",
+    bpe='sentencepiece',
+    sentencepiece_model='./fairseq/spmModels/spm.ja.nopretok.model',
+    # is_gpu=True
+)
+
+ja2en.cuda()
+
+app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/", methods = ['POST'])
+@cross_origin()
+
+def sendImage():
+    tic = time.perf_counter()
+    data = request.get_json()
+    message = data.get("message")
+    content = data.get("content").strip('﻿').strip();
+
+    if (message == "close server"):
+        shutdown_server()
+        return
+
+    if (message == "translate sentences"):
+        t = ja2en.translate(content)
+
+        toc = time.perf_counter()
+        print("Request: ", content)
+        print("Translation (",round(toc-tic,2), "s): ", t)
+
+        return json.dumps(t)
+
+    if (message == "translate batch"):
+        batch = data.get("batch")
+        if isinstance(batch, list):
+            batch = [s.strip('﻿').strip() for s in batch]
+
+            translated = [
+                ja2en.translate(s)
+                for s in batch
+            ]
+
+        toc = time.perf_counter()
+        print("Request: ", batch)
+        print("Translation (",round(toc-tic,2), "s): ", translated)
+
+        return json.dumps(translated)
+
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+if __name__ == "__main__":
+    # monkey patch cli banner
+    from flask import cli
+    cli.show_server_banner = lambda *_: None
+
+    port = int(sys.argv[1])
+    print(f"Running server on port {port}")
+    app.run(host='127.0.0.1', port=port)
